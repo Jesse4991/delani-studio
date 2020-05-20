1ac7c4ad676a2c0c484e9d6cb90f9c683169b59c
jesse <jessekibungei@gmail.com>
2020-05-20 16:24:28 +0300

    added social icons and background images

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+  $(".icon").click(function(){
+    $(".show").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".icon2").click(function(){
+    $(".show2").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".icon3").click(function(){
+    $(".show3").toggle();
+  });
+});
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+function myFunction() {
+  alert("We have received your message. Thank you for contacting us.");
+}
